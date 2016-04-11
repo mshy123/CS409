d076103abd215e61a4ba997494b7961c5816f288
Hyunho Ha <mshy123@kaist.ac.kr>
2016-04-11 11:20:25 +0900

    Syslog packet add

--- spark/spark_consumer.sh
+++ spark/spark_consumer.sh
@@ -1,1 +1,1 @@
-sudo ./bin/spark-submit --class "com.mycompany.app.App" --jars First_app-1.0-SNAPSHOT-jar-with-dependencies.jar First_app-1.0-SNAPSHOT.jar 127.0.0.1:2181 01 apache_access_topic 1
+sudo ./bin/spark-submit --class "com.mycompany.app.App" --jars First_app-1.0-SNAPSHOT-jar-with-dependencies.jar First_app-1.0-SNAPSHOT.jar 127.0.0.1:2181 01 apache_access_topic,syslog_topic 1
