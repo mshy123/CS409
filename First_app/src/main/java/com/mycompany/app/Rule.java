f3b5fd8b0fd1ac30f48b24ebbe143539275aa267
goodahn <qiqi67@kaist.ac.kr>
2016-05-10 19:52:25 +0900

    complete simple functions : check, update, checkComplete

--- First_app/src/main/java/com/mycompany/app/Rule.java
+++ First_app/src/main/java/com/mycompany/app/Rule.java
@@ -11,29 +11,43 @@ public class Rule {
 	
 	private long duration;
 	
-	private ArrayList<String> types;
+	private ArrayList<Tuple> types;
 	
-	private ArrayList<String> checkedTypes;
+	private ArrayList<Tuple> checkedTypes;
 	
 	private Boolean ordered;
 	
 	private ArrayList<String> attributeList;
 	
-	public Rule( String name_ , long duration_ , Boolean ordered_, ArrayList<String> types_) {
+	public Rule( String name_ , long duration_ , Boolean ordered_, ArrayList<Tuple> types_) {
 		name = name_;
 		duration = duration_;
 		ordered = ordered_;
 		types = types_;
 	}
 	
-	public Boolean check( Rule rule1) {
-		return true;
+	public Boolean check( Tuple type) {
+		if( ordered) {
+			if( type.typeName == types.get( checkedTypes.size() ).typeName ) {
+				return true;
+			}
+		} else {
+			if( types.contains( type.typeName ) && !checkedTypes.contains( type.typeName ) ) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
-	public void update( String type ) {
+	public void update( Tuple type ) {
 		//updat 할 때 쳌트 타입스에 하나도 없으면 벌스타임을 추가하면 될듯?
 		if( checkedTypes.isEmpty() ) {
 			birthTime = System.currentTimeMillis();
 		}
+		checkedTypes.add(type);
+	}	
+	
+	public Boolean checkComplete() {
+		return checkedTypes.size == types.size();
 	}
 }
