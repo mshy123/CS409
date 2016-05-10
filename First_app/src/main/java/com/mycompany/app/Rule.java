19f3d90e3a3b32396d3ed033ac7b9759aa5612d5
goodahn <qiqi67@kaist.ac.kr>
2016-05-11 01:57:23 +0900

    modify check function return result_code

--- First_app/src/main/java/com/mycompany/app/Rule.java
+++ First_app/src/main/java/com/mycompany/app/Rule.java
@@ -3,6 +3,12 @@ package com.mycompany.app;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import scala.util.parsing.json.JSON;
+
 public class Rule {
 	
 	private String name;
@@ -19,6 +25,8 @@ public class Rule {
 	
 	private ArrayList<String> attributeList;
 	
+	public enum resultCode { UPDATE, TIMEOVER, COMPLETE, FAIL } 
+	
 	public Rule( String name_ , long duration_ , Boolean ordered_, ArrayList<Tuple> types_) {
 		name = name_;
 		duration = duration_;
@@ -26,18 +34,58 @@ public class Rule {
 		types = types_;
 	}
 	
-	public Boolean check( Tuple type) {
+	public resultCode check( Tuple type) {
+		if( System.currentTimeMillis() < birthTime + duration ) {
+			return resultCode.TIMEOVER;
+		}
+		if( checkedTypes.size() == types.size() ) {
+		 return resultCode.COMPLETE;
+		}
 		if( ordered) {
 			if( type.typeName == types.get( checkedTypes.size() ).typeName ) {
-				return true;
+				if( attributeList.size() == 0 ) {
+					return resultCode.UPDATE;
+				} else {
+					JSONParser parser = new JSONParser();
+					try {
+						JSONObject compareJson = (JSONObject)parser.parse(checkedTypes.get(0).content);
+						JSONObject targetJson = (JSONObject)parser.parse(type.content);
+						for( int i = 0 ; i < attributeList.size() ; i ++ ) {
+							if( targetJson.get(attributeList.get(i)).toString() 
+									!= compareJson.get(attributeList.get(i)).toString() ){
+								return resultCode.FAIL;
+							}
+						}
+						return resultCode.UPDATE;
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		} else {
 			if( types.contains( type.typeName ) && !checkedTypes.contains( type.typeName ) ) {
-				return true;
+				if( attributeList.size() == 0 ) {
+					return resultCode.UPDATE;
+				} else {
+					JSONParser parser = new JSONParser();
+					try {
+						JSONObject compareJson = (JSONObject)parser.parse(checkedTypes.get(0).content);
+						JSONObject targetJson = (JSONObject)parser.parse(type.content);
+						for( int i = 0 ; i < attributeList.size() ; i ++ ) {
+							if( targetJson.get(attributeList.get(i)).toString() 
+									!= compareJson.get(attributeList.get(i)).toString() ){
+								return resultCode.FAIL;
+							}
+						}
+						return resultCode.UPDATE;
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
-		return false;
-	}
+		return resultCode.FAIL;
+	 }
 	
 	public void update( Tuple type ) {
 		//updat 할 때 쳌트 타입스에 하나도 없으면 벌스타임을 추가하면 될듯?
@@ -46,8 +94,4 @@ public class Rule {
 		}
 		checkedTypes.add(type);
 	}	
-	
-	public Boolean checkComplete() {
-		return checkedTypes.size == types.size();
-	}
 }
--- First_app/src/main/java/com/mycompany/app/Tuple.java
+++ First_app/src/main/java/com/mycompany/app/Tuple.java
@@ -3,8 +3,10 @@ package com.mycompany.app;
 public class Tuple {
 	public String typeName;
 	public String content;
+	public long time;
 	public Tuple( String typeName_ , String content_ ) {
 		typeName = typeName_;
 		content = content_;
+		time = System.currentTimeMillis();
 	}
 }
